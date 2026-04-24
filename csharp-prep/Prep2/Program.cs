fc81f4cecb9d7e8dbadf5d897bde05adcc942d91
SPatten3 <spencer.patten@hotmail.com>
2026-04-23 22:02:14 -0600

    Prep 1 and Prep 2 assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter the Grade:  ");
+        string strGrade = Console.ReadLine();
+        double dblGrade = double.Parse(strGrade);
+
+        string letterGrade;
+
+        double ones = dblGrade % 10;
+        string modifier;
+        
+        if (dblGrade >= 90)
+        {
+            letterGrade = "A";
+        } else if (dblGrade >= 80)
+        {
+            letterGrade = "B";
+        } else if (dblGrade >= 70)
+        {
+            letterGrade = "C";
+        } else if (dblGrade >= 60)
+        {
+            letterGrade = "D";
+        } else
+        {
+            letterGrade = "F";
+        }
+
+        if (dblGrade < 93 && dblGrade >= 60)
+        {
+            if (ones >= 0 && ones < 3)
+            {
+                modifier = "-";
+            } else if (ones >= 7)
+            {
+                modifier = "+";
+            } else
+            {
+                modifier = "";
+            }
+        } else
+        {
+            modifier = "";
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}{modifier}.");
     }
 }
