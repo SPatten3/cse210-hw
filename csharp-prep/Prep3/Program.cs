80ebfc52250fb547c5be1b4a60895be52150fac8
SPatten3 <spencer.patten@hotmail.com>
2026-04-25 20:27:30 -0600

    Prep 3 w/ stretch challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string choice = "Y";
+
+        while (choice == "Y")
+        {
+            
+            Random rng = new Random();
+            int magicNumber = rng.Next(0,100);
+
+            int guessNumber = 0;
+            int userGuess = -1;
+
+            Console.WriteLine("I have a magic number, and you must guess it.");
+
+            while (userGuess != magicNumber)
+            {
+                
+            Console.Write("Enter your guess:  ");
+            userGuess = int.Parse(Console.ReadLine());
+            guessNumber += 1;
+
+            if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                } else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                } else
+                {
+                    Console.WriteLine($"That is correct! You got in in {guessNumber} guesses");
+                }
+
+            }
+
+            Console.Write("Would you like to play again? (Y/N).  ");
+            choice = Console.ReadLine();
+
+
+        }
+        
+
+
     }
 }
