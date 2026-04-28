0690442dc0cf006f1a3799878ad9e8aa7a6cacf0
SPatten3 <spencer.patten@hotmail.com>
2026-04-27 19:34:29 -0600

    Messing around w/ sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        int age;
+
+        do
+        {
+            Console.Write("Enter your age:  ");
+            age = int.Parse(Console.ReadLine());
+        } while(age < 0 || age > 125);
+
     }
 }
