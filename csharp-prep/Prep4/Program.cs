ce9563cd375699c3cf2b3d8d4d0fc6b85cf3041b
SPatten3 <spencer.patten@hotmail.com>
2026-04-29 14:33:12 -0600

    Prep 4 assignment done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> ints = new List<int>();
+        int newInt;
+        int sum = 0;
+        int largestNumber = -100000;
+        int smallestNumber = 999999;
+        Console.WriteLine("Enter a list of numbers. Type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter a whole number: ");
+            newInt = int.Parse(Console.ReadLine());
+            sum += newInt;
+            ints.Add(newInt);
+
+            if (newInt > largestNumber)
+            {
+                largestNumber = newInt;
+            }
+            if (newInt < smallestNumber && newInt > 0)
+            {
+                smallestNumber = newInt;
+            }
+
+        } while(newInt != 0);
+
+        double average = (sum/ints.Count);
+        
+        Console.WriteLine($"The average is {average}.");
+        Console.WriteLine($"The sum is {sum}.");
+        Console.WriteLine($"The largest number is {largestNumber}.");
+        Console.WriteLine($"The smallest positive number is {smallestNumber}.");
+        
+
     }
 }
