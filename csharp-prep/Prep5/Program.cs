364165a81c016c913639589c67f1dd34c061b284
SPatten3 <spencer.patten@hotmail.com>
2026-04-30 10:39:38 -0600

    Prep 5 w/ PromptUserName

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,8 +6,16 @@ class Program
     {
         Console.WriteLine("Welcome to the program!");
     }
+
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name:  ");
+        string name = Console.ReadLine();
+        return name;
+    }
     static void Main(string[] args)
     {
         DisplayWelcome();
+        string name = PromptUserName();
     }
 }
