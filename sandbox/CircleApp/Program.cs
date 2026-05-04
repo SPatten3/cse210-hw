8ca99ad21df200735d2794d61b1d223dadf5d3f0
SPatten3 <spencer.patten@hotmail.com>
2026-05-04 14:42:52 -0600

    Used circle class in Program.cs

--- sandbox/CircleApp/Program.cs
+++ sandbox/CircleApp/Program.cs
@@ -2,6 +2,9 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        Circle myCircle = new Circle();
+        myCircle.SetRadius(10);
+        Console.WriteLine(myCircle.GetArea());
+
     }
 }
