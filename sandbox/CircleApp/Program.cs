e0ac54f3ad3b6ea72cb0998ea4c9bc4e42264b90
SPatten3 <spencer.patten@hotmail.com>
2026-05-04 14:58:30 -0600

    Made cylinder class

--- sandbox/CircleApp/Program.cs
+++ sandbox/CircleApp/Program.cs
@@ -6,5 +6,11 @@
         myCircle.SetRadius(10);
         Console.WriteLine(myCircle.GetArea());
 
+        Cylinder myCylinder = new Cylinder();
+        myCylinder._circle = new Circle();
+        myCylinder._circle.SetRadius(10);
+        myCylinder._height = 20;
+        Console.WriteLine(myCylinder.GetCylinderVolume());
+
     }
 }
